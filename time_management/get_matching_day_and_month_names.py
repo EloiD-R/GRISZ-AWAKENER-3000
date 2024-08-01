47bd5032c6e76637274a5e3768def11bf4f75459
Eloi D--R <eloid--r@pop-os.localdomain>
2024-08-01 10:36:19 +0200

    fixed the time getting system so i can actually have name to days and months instead of None

---
+++ time_management/get_matching_day_and_month_names.py
@@ -0,0 +1,43 @@
+"""
+Ugly but necessary matching code
+"""
+
+"""GLOBAL VARIABLES (kinda)"""
+# Necessary tables
+month_names_and_numbers = [1, "january", 2, "february", 3, "march", 4, "april", 5, "may", 6, "june", 7, "july", 8, "august", 9, "september", 10, "october", 11, "november", 12, "december"]
+day_names_and_numbers = [1, "monday", 2, "tuesday", 3, "wednesday", 4, "thursday", 5, "friday", 6, "saturday", 7, "sunday"]
+
+"""FUNCTIONS"""
+def get_day_name_based_on_day_number(number):
+    # Using time_getter you get lists so we need to deal with it
+    if type(number) is list:
+         number = number[0]
+
+    for index in range(len(day_names_and_numbers)):
+        if number == day_names_and_numbers[index]:
+            return day_names_and_numbers[index + 1]
+
+
+def get_month_name_based_on_month_number(number):
+    # Using time_getter you get a str in a list so we need to deal with it
+    if type(number) is list:
+            number = number[0]
+            number = int(number)
+
+    for index in range(len(month_names_and_numbers)):
+        if number == month_names_and_numbers[index]:
+            return month_names_and_numbers[index + 1]
+
+
+
+"""CODE"""
+if __name__ == "__main__":
+    for day_number in range(1, 8):
+        print(get_day_name_based_on_day_number(day_number), end=" ")
+
+    print("\n")
+
+    for month_number in range(1, 13):
+        print(get_month_name_based_on_month_number(month_number), end=" ")
+
+    print("\n")
